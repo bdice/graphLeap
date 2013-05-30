1a7399a8f8f66a86ab0b446d9f237044e4a605a6
Bradley Dice <bdice@bradleydice.com>
2013-05-30 10:53:13 -0500

    First commit.

---
+++ src/graphLeap.java
@@ -0,0 +1,13 @@
+// graphLeap main class
+
+public class graphLeap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to graphLeap!");
+	}
+
+}
