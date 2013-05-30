d90b8f5eba34f53dd9075f5a28ce007df18646ec
bzoomer16 <bdice@bradleydice.com>
2013-05-30 16:29:27 -0500

    Added GSON Library.

--- src/graphLeap.java
+++ src/graphLeap.java
@@ -1,13 +1,19 @@
 // graphLeap main class
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class graphLeap {
 
 	/**
 	 * @param args
+	 * @throws TwitterException 
+	 * @throws IllegalStateException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to graphLeap!");
+	public static void main(String[] args) throws IllegalStateException, TwitterException {
+		Twitter twitter = TwitterFactory.getSingleton();
+		System.out.println(twitter.getHomeTimeline());
 	}
 
 }
